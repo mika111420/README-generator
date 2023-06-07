3391319832c8616385e52ea140309ec64d9f0f27
Mikayla <user@users-MBP.attlocal.net>
2023-06-07 17:20:15 -0400

    code 1

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,120 @@
 // TODO: Include packages needed for this application
 const inquirer=require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Enter project title: ',
+    
+    },
+    // {
+    //     type: 'input'
+    //     name: 'description',
+    //     message: 'Enter project description',
+    // }
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'Enter installation instructions:',
+      },
+      {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter usage information:',
+      },
+      {
+        type: 'input',
+        name: 'contribution',
+        message: 'Enter contribution guidelines:',
+      },
+      {
+        type: 'input',
+        name: 'tests',
+        message: 'Enter test instructions:',
+      },
+      {
+        type: 'list',
+        name: 'license',
+        message: 'Choose a license for your application:',
+        choices: ['MIT', 'Apache', 'GPL', 'None'],
+      },
+      {
+        type: 'input',
+        name: 'github',
+        message: 'Enter your GitHub username:',
+      },
+      {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address:',
+      },
+    ];
+    
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+
+
+function generateReadme(data) {
+    return `
+  # ${data.title}
+  
+  ## Description
+  ${data.description}
+  
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [License](#license)
+  - [Questions](#questions)
+  
+  ## Installation
+  ${data.installation}
+  
+  ## Usage
+  ${data.usage}
+  
+  ## Contributing
+  ${data.contribution}
+  
+  ## Tests
+  ${data.tests}
+  
+  ## License
+  This application is covered under the ${data.license} license.
+  
+  ## Questions
+  GitHub: [${data.github}](https://github.com/${data.github})
+  Email: ${data.email}
+  `;
+  }
+
+
+function writeToFile(fileName, data) {
+    fs.writeToFile(fileName, data, (error) =>{
+        if(error) {
+            console.error('Error creating README file:', error);
+        } else{
+            console.log('README file has been generated successfully!');
+        }
+    })
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+    .prompt(questions)
+    .then((answers) => {
+        const readmeContent = generateMarkdown(answers);
+        writeToFile('README.md', readmeContent);
+    })
+    .catch((error)=> {
+        console.error('An error occured',error)
+    });
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,19 +1,60 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// Function that returns a license badge based on the license
+function renderLicenseBadge(license) {
+  if (!license) {
+    return '';
+  }
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+  // Map license names to badge URLs
+  const licenseBadges = {
+    MIT: '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)',
+    Apache: '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)',
+    GPL: '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)',
+  };
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+  return licenseBadges[license] || '';
+}
 
-// TODO: Create a function to generate markdown for README
+// Function that returns the license link
+function renderLicenseLink(license) {
+  if (!license) {
+    return '';
+  }
+
+  // Map license names to license URLs
+  const licenseLinks = {
+    MIT: '[MIT](https://opensource.org/licenses/MIT)',
+    Apache: '[Apache](https://opensource.org/licenses/Apache-2.0)',
+    GPL: '[GPL v3](https://www.gnu.org/licenses/gpl-3.0)',
+  };
+
+  return licenseLinks[license] || '';
+}
+
+// Function that returns the license section of the README
+function renderLicenseSection(license) {
+  if (!license) {
+    return '';
+  }
+
+  return `
+## License
+This application is covered under the ${license} license. ${renderLicenseLink(license)}
+`;
+}
+
+// Function to generate markdown for the README
 function generateMarkdown(data) {
   return `# ${data.title}
 
+${renderLicenseBadge(data.license)}
+
+## Description
+${data.description}
+
+## Table of Contents
+- [License](#license)
+
+${renderLicenseSection(data.license)}
 `;
 }
 
